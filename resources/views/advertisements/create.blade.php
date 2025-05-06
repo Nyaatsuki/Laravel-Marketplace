029decfc45a88cbfc29be9906fd0bc201ec86a7e
Nyaatsuki <puttinpanfu@gmail.com>
2025-05-06 15:41:02 +0200

    the like.. form or... something

--- resources/views/advertisements/create.blade.php
+++ resources/views/advertisements/create.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
-    <h1>This is where your shit goes, Luna</h1>
-    <h3>but for now just imagine there's a Form here</h3>
+    <form method="POST" action="/register">
+        @csrf
+
+        <!-- Put shit here -->
+
+    </form>
 </x-layout>
