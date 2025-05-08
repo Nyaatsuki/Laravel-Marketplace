4783afbace98d87aac6d7a44ea9fc9486a3dbd0e
Nyaatsuki <puttinpanfu@gmail.com>
2025-05-08 15:09:34 +0200

    more prep

--- resources/views/advertisements/create.blade.php
+++ resources/views/advertisements/create.blade.php
@@ -1,8 +1,20 @@
 <x-layout>
-    <form method="POST" action="/register">
-        @csrf
+    <section class="advertisement-form">
+        <div>
+            <form method="POST" action="/register">
+                @csrf
 
-        <!-- Put shit here -->
-
-    </form>
+                <label for="name">Title</label><br>
+                <input type="text" id="name" name="name" value="{{ old('name') }}"><br>
+                <br>
+                <label for="description">Description</label><br>
+                <input type="text" id="description" name="description" value="{{ old('description') }}"><br>
+                <br>
+                <label for="price">Price</label><br>
+                <input type="text" id="price" name="price" value="{{ old('price') }}"><br>
+                <br>
+                <!-- Image input -->
+            </form>
+        </div>
+    </section>
 </x-layout>
