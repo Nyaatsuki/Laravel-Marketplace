4f112a660f41b418bbf32596e5323412f5f483e0
Nyaatsuki <puttinpanfu@gmail.com>
2025-06-19 14:35:39 +0200

    write todo's

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,7 @@ class PostController extends Controller
     }
 
     public function store(){
-        // it has to do the thing
+        // SHIT I NEED TO PUSH TO THE DB LIKE THE DUMB BITCH I AM: Ad Title, Ad Description, Ad price, Ad image
+        // Grab the info from the inputs and store them in the database, then show them on the main page
     }
 }
