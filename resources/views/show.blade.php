d33919c919c0bbdbb6524d43c1c3c6b7fca495bb
Nyaatsuki <puttinpanfu@gmail.com>
2025-10-28 10:35:18 +0100

    Fix show page Category tag link

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,7 +2,7 @@
     <div class="show-container">
         <img src="{{ $advertisement->image }}" class="big-img">
         <h5 class="user-name">Posted by: {{ $advertisement->author->name }}</h5>
-        <a href="/category/{{ $advertisement->category->slug }}" class="category-tag">{{ $advertisement->category->name }}</a>
+        <a href="/?category={{ $advertisement->category->slug }}&{{ http_build_query(request()->except('category')) }}" class="category-tag">{{$advertisement->category->name}}</a>
         <h1 class="show-title">{{$advertisement->title}}</h1>
         <p>€{{ number_format($advertisement->price, 2) }}</p>
         <p>{!! $advertisement->body !!}</p>
